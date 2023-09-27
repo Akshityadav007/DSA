31a64da61fa25eaaa2f5e226c35f767e5b6e2a7a
Akshit Yadav <amahacker007@gmail.com>
2023-09-27 11:02:50 +0530

    commit

--- Strings/MinimumTimeDifference/minimumTimeDiff.cpp
+++ Strings/MinimumTimeDifference/minimumTimeDiff.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Problem  || Leetcode : 539
+// # Couldn't solve this, although attempted it
 /*
     -> Given a list of 24-hour clock time points in "HH:MM" format.
     -> Return the minimum minutes difference between any two time-points in the list.
@@ -12,38 +13,58 @@ using namespace std;
     -> First convert all time in hours
     -> Sort the array (because minimum will be achieved by difference of adjacent time)
     -> Get the minimum of the difference.
+
+    -> There are two types of differences:
+        - Moving in minor - segment
+        - Moving in major - segmenet
+    -> But which is the minor segment ?
+    -> We take both and take minimum among them to be answer
+    -> Conider one movement clockwise and the other anti - clockwise
+    
+    -> Basically issue comes with day and night
+        - We can consider day after night or night after day
+        - Both are valid, but which is the minimum consideration ?
+    
 */
 
-int findMinDifference(vector<string> &s){
-    // store all time in minutes
-    vector<int> v;
+vector<int> getHours(vector<string> &s){
+    vector<int> hrs;
     for(int i = 0;i < s.size();i++){
-        // get hours
-        int hours = stoi(s[i].substr(0,2));             //  stoi: used to convert string to integer
-        // get minutes
-        int minutes = stoi(s[i].substr(3,2));           //  syntax: substr(from,no. of steps);
-      
-        // convert the time in minutes
-        int totalTime;
-        cout << hours << " " << minutes << endl;
-        totalTime = (hours * 60) + minutes;
-        cout << totalTime << endl;
-        v.push_back(totalTime);
+        int h = stoi(s[i].substr(0,2));
+        hrs.push_back(h);
+    }
+    return hrs;
+}
+
+vector<int> getMinutes(vector<string> &s){
+    vector<int> mins;
+    for(int i = 0;i < s.size();i++){
+        int m = stoi(s[i].substr(3,2));
+        mins.push_back(m);
+    }
+    return mins;
+}
+
+
+int antiClockWiseConvert(int hours, int minutes){
+    return ((24 - hours)* 60) + minutes;
+}
+
+int findMinDifference(vector<string> &s){
+    int minDiff = 0;
+
+
+    for(int i = 1;i < s.size();i++){
+
     }
 
-    // sort all time
-    sort(v.begin(),v.end());
-    int minDiff = INT_MAX;
-    for(int i = 1;i < v.size();i++){
-        minDiff = min(minDiff, v[i] - v[i - 1]);
-    }
     return minDiff;
 }
 
 int main(){
     vector<string> v;
-    v.push_back("23:59");
-    v.push_back("00:00");
+    v.push_back("12:35");
+    v.push_back("00:10");
 
     cout << "The minimum time difference is: " << findMinDifference(v) << endl;
 
