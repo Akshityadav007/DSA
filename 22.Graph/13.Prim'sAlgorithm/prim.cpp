0b558aeb512abdf1071e0c077e81db74c4e484cd
Akshit Yadav <amahacker007@gmail.com>
2024-08-10 11:04:00 +0530

    commit

---
+++ 22.Graph/13.Prim'sAlgorithm/prim.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+// Spanning Tree (ST)
+/*
+    -> Convert graph to a tree (no cycle).
+    -> Every node of the tree should be reachable by every other node.
+    -> Tree would have 'n' nodes and 'n - 1' edges.
+
+    -> MST is a ST where the sum of all weights of edges in the tree should be minimum.
+    -> There are different algo's to make a minimum spanning tree:
+*/
+
+// 1) Prim's Algorithm
+/*
+    -> This algo states that we'll build the tree from scratch.
+
+*/
+
+
+int main(){
+
+    return 0;
+}
